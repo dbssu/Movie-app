3d005c5236d7002c4006349dfc0bc6960008d2ba
Yoonsu Kang <kangys0725@naver.com>
2019-04-06 18:31:47 +0900

    Add functional component

--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,31 @@
-import React, { Component  } from "react";
+import React from "react";
 import './Movie.css';
 import PropTypes from 'prop-types';
 
-class Movie extends Component{
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie ({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src={this.props.poster} alt="Movie Poster" />
-        )
-    }
+function MoviePoster({poster}){ // functional 컴포넌트 -> 위 class MoviePoster과 같은 역할 둘 다 img와 props를 return한다.
+    return(
+        <img src={poster} alt="Movie Poster"/>
+    )
 }
+
+Movie.protoTypes = {
+    title:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
+}
+
+MoviePoster.prototype = {
+    poster: PropTypes.string.isRequired
+}
+
+
 export default Movie;
