16aa514c1ae7c01d203b853ac52dbc54a943a206
Yoonsu Kang <kangys0725@naver.com>
2019-04-09 13:47:28 +0900

    Updating Movie

--- src/App.js
+++ src/App.js
@@ -15,7 +15,13 @@ class App extends Component {
   
     _renderMovies = () => { // _를 쓰는 이유는 리액트 자체 기능과 나의 기능에 차이를 두기 위해서이다 !
       const movies = this.state.movies.map(movie => {
-        return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+        return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        />
       })
       return movies
     }
